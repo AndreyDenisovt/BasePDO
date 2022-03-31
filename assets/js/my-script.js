d3ad6ebf5036c7ec63d3cce883c9c5fb3c785a6c
AndreyDenisovt <andreydenisov@inbox.ru>
2022-03-31 17:57:23 +0300

    qw

---
+++ assets/js/my-script.js
@@ -0,0 +1,18 @@
+
+const new_project_btn = document.querySelector(".new-project-btn");
+const new_project_form = document.querySelector("form.new-project-form");
+
+
+new_project_btn.addEventListener("click",function() {
+
+    if(!new_project_btn.classList.contains("active")){
+        new_project_btn.classList.add("active");
+        new_project_form.classList.add("active");    
+    }else{
+        new_project_btn.classList.remove("active");
+        new_project_form.classList.remove("active");
+    }
+    
+});
+
+console.log("js");
--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" type="text/css" href="assets/my-style.css">    
     
     <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script defer src="assets/js/my-script.js"></script>
+
 </head>
 <body>
 
@@ -33,60 +35,96 @@ $opt = [
 
 $dbh = new PDO($dsn, $user, $password,$opt);
 
-$show_all_tables = function ($dbh){
-    $query_ST = "SHOW TABLES";
+$show_all_names = function ($dbh){
+    $query_ST = "SELECT `site-name`, `cms-adress`,`site-login`,`site-password` FROM `klients-projects`";
     $sth = $dbh->prepare($query_ST);
-    $sth->execute();
-    return $sth->fetch(PDO::FETCH_ASSOC);
+    $sth->execute();    
+    return $sth->fetchAll(PDO::FETCH_ASSOC);
+    
 };
+$show_names_pass = $show_all_names($dbh);
 /* 
 * create new project\site(table)
-* $query_CT = "CREATE TABLE `testDB`.`klient-project-name` ( `id` INT NOT NULL AUTO_INCREMENT , `site-name` VARCHAR(255) , `site-login` TINYTEXT NOT NULL , `site-password` TINYTEXT NOT NULL , `ftp-host` TINYTEXT NOT NULL , `ftp-login` TINYTEXT NOT NULL , `ftp-password` TINYTEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB";
-* $query_II = "INSERT INTO `klient-project-name`( `site-name`, `site-login`, `site-password`, `ftp-host`, `ftp-login`, `ftp-password`) VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]')";
-* show all project
-* $query_ST = "SHOW TABLES";
-
-
-
+* $query_CT = "CREATE TABLE `testDB`.`klients-projects` ( `id` INT NOT NULL AUTO_INCREMENT , `site-name` VARCHAR(255) , `csm-adress` VARCHAR(255) , `site-login` VARCHAR(255)  , `site-password` VARCHAR(255)  , `ftp-host` VARCHAR(255)  , `ftp-login` VARCHAR(255)  , `ftp-password` VARCHAR(255)  ,`db-name` VARCHAR(255), `db-login` VARCHAR(255) ,`db-password` VARCHAR(255) ,`more-info` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB";
+* $query_II = "INSERT INTO `klients-projects`( `site-name`, `cms-adress`, `site-login`, `site-password`, `ftp-host`, `ftp-login`, `ftp-password`,`db-name`, `db-login`,`db-password`,`more-info`) VALUES ('site name', 'cms url', 'site login', 'site password', 'ftp url', 'ftp login', 'ftp password','db name', 'db login','db password','more info')";
+ 
 $sth = $dbh->prepare($query);
 $sth->execute();
 */
 
-$query_II = "INSERT INTO `klient-project-name`( `site-name`, `site-login`, `site-password`, `ftp-host`, `ftp-login`, `ftp-password`) VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]')";
 
-$query_TR = "SELECT * FROM `klient-project-name`";
-
-$sth = $dbh->prepare($query_TR);
-
-$sth->execute();
-$data = $sth->fetchAll();
+                                            
+// $sth = $dbh->prepare($query_II);
+// $sth->execute();
+// $data = $sth->fetchAll();
 
 
+if (strlen($_REQUEST["new-project-input"]) > 0  && strlen($_REQUEST["site-name"]) > 0 ){
+    array_pop($_REQUEST);
+    $str_values = implode("','",$_REQUEST);
+    $str_values = "'".$str_values."'";
+    var_dump($str_values);
+    $query_create_new_project = "INSERT INTO `klients-projects`( `site-name`, `cms-adress`, `site-login`, `site-password`, `ftp-host`, `ftp-login`, `ftp-password`,`db-name`, `db-login`,`db-password`,`more-info`) VALUES ($str_values)";
+    $sth = $dbh->prepare($query_create_new_project);
+    $sth->execute();    
+}
+echo "<pre>"; 
+print_r($_REQUEST);
 
-echo "<pre>";
-//print_r($data);
-echo "</pre>"
+echo "</pre>";
 /******/
+
 ?>
 <main>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 m-2">
+                <div class="d-flex justify-content-center h2">
+                    <a href="/">SYStem KEYs</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="btn btn-success new-project-btn">Создать новый проект</div>
+                    <form class="new-project-form active" action="">                        
+                        <input type="text" name="site-name" placeholder="site-name" value="">
+                        <input type="text" name="cms-adress" placeholder="cms-adress" value="">
+                        <input type="text" name="site-login" placeholder="site-login" value="">
+                        <input type="text" name="site-password" placeholder="site-password" value="">
+                        <input type="text" name="ftp-host" placeholder="ftp-host" value="">
+                        <input type="text" name="ftp-login" placeholder="ftp-login" value="">
+                        <input type="text" name="ftp-password" placeholder="ftp-password" value="">
+                        <input type="text" name="db-name" placeholder="db-name" value="">
+                        <input type="text" name="db-login" placeholder="db-login" value="">
+                        <input type="text" name="db-password" placeholder="db-password" value="">
+                        <textarea name="more-info"></textarea>
+                        <button type="submit" name="new-project-input" value="1" class="btn btn-info">Сохранить</button>
+                    </form>
+            </div>
+        </div>
+    </div>
+    <hr>
 <div class="container">
     <div class="row">
-        <div class="col-md-3 bg-info">
+        <div class="col-md-3 bg-light d-flex align-items-center flex-column">
             <?php
-                foreach($show_all_tables($dbh) as $name){
-                        echo "<p>".$name."</p>";
+                foreach($show_names_pass as $name){                    
+                    echo "<div class='m-1 p-1'>".$name["site-name"]."</div>";
                 }
             ?>
         </div>     
     <?php
-    if ($data > 0){       
+    if ($show_names_pass > 0){       
     ?>
         <div class="col-md-9">
-            <table class="table table-striped table-bordered">
+            <table class="table table-striped table-bordered text-center">
                 <tr>
                 <?php
-                    foreach($data[0] as $k=>$v):?>
+                    foreach($show_names_pass[0] as $k=>$v):?>
                         <th>
                             <?php
                             echo $k;
@@ -97,7 +135,7 @@ echo "</pre>"
                 </tr>
                 
                 <?php
-                foreach($data as $array){?>
+                foreach($show_names_pass as $array){?>
                 <tr>
                 <?php 
                     foreach($array as $value){?>        

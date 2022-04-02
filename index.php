a5f25e1913978995f3a614f422a9097941702028
AndyDenny <tp0yah@gmail.com>
2022-04-02 14:30:58 +0300

    rename all columns in table

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $opt = [
 $dbh = new PDO($dsn, $user, $password,$opt);
 
 $show_all_names = function ($dbh){
-    $query_ST = "SELECT `id`, `site-name`, `cms-adress`,`site-login`,`site-password` FROM `klients-projects`";
+    $query_ST = "SELECT id, site_name, cms_adress,site_login,site_password FROM klients_projects";
     $sth = $dbh->prepare($query_ST);
     $sth->execute();    
     return $sth->fetchAll(PDO::FETCH_ASSOC);
@@ -47,28 +47,28 @@ $show_all_names = function ($dbh){
 $show_names_pass = $show_all_names($dbh);
 
  
-if (strlen($_GET["delete-row"]) > 0 ){
-        $row_for_delete = $_GET["delete-row"];
-        $query_DT = "DELETE FROM `klients-projects` WHERE `id`= $row_for_delete";
+if (strlen($_GET["delete_row"]) > 0 ){
+        $row_for_delete = $_GET["delete_row"];
+        $query_DT = "DELETE FROM klients_projects WHERE id= $row_for_delete";
         $sth = $dbh->prepare($query_DT);
         $sth->execute();   
          
 }
 
-if (strlen($_GET["details-row"]) > 0 ){
-    $row_for_details = $_GET["details-row"];
-    $query_DR = "SELECT * FROM `klients-projects` WHERE `id`= $row_for_details";
+if (strlen($_GET["details_row"]) > 0 ){
+    $row_for_details = $_GET["details_row"];
+    $query_DR = "SELECT * FROM klients_projects WHERE id= $row_for_details";
     $sth = $dbh->prepare($query_DR);
     $sth->execute();   
     $details_row = $sth->fetchAll(PDO::FETCH_ASSOC);
 }
 
-if (strlen($_GET["save-edit-row"]) > 0){
+if (strlen($_GET["save_edit_row"]) > 0){
     array_pop($_GET);
     $id = $_GET["id"]; ///???
 
-    //UPDATE `klients-projects` SET `ftp-password` = 'assword', `db-login` = 'db-log' WHERE `klients-projects`.`id` = 7 
-    $query_UT = "UPDATE `klients-projects` SET `site-name`=':site-name', `cms-adress`=':cms-adress', `site-login`=':site-login', `site-password`=':site-password', `ftp-host`=':ftp-host', `ftp-login`=':ftp-login', `ftp-password`=':ftp-password',`db-name`=':db-name', `db-login`=':db-login',`db-password`=':db-password',`more-info`=':more-info' WHERE `id`= $id";
+    //UPDATE klients_projects SET ftp_password = 'assword', db_login = 'db-log' WHERE id = 7 
+    $query_UT = "UPDATE klients_projects SET site_name=':site-name', cms_adress=':cms-adress', site_login=':site-login', site_password=':site-password', ftp_host=':ftp-host', ftp_login=':ftp-login', ftp_password=':ftp-password', db_name=':db-name', db_login=':db-login',db_password=':db-password',more_info=':more-info' WHERE id= $id";
     $sth = $dbh->prepare($query_UT);
     foreach($_GET as $key=>$value){
         $sth->bindValue($key,$value);
@@ -79,19 +79,19 @@ if (strlen($_GET["save-edit-row"]) > 0){
 
 /* 
 * create new project\site(table)
-* $query_CT = "CREATE TABLE `testDB`.`klients-projects` ( `id` INT NOT NULL AUTO_INCREMENT , `site-name` VARCHAR(255) , `csm-adress` VARCHAR(255) , `site-login` VARCHAR(255)  , `site-password` VARCHAR(255)  , `ftp-host` VARCHAR(255)  , `ftp-login` VARCHAR(255)  , `ftp-password` VARCHAR(255)  ,`db-name` VARCHAR(255), `db-login` VARCHAR(255) ,`db-password` VARCHAR(255) ,`more-info` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB";
-* $query_II = "INSERT INTO `klients-projects`( `site-name`, `cms-adress`, `site-login`, `site-password`, `ftp-host`, `ftp-login`, `ftp-password`,`db-name`, `db-login`,`db-password`,`more-info`) VALUES ('site name', 'cms url', 'site login', 'site password', 'ftp url', 'ftp login', 'ftp password','db name', 'db login','db password','more info')";
+* $query_CT = "CREATE TABLE `testDB`.klients_projects ( id INT NOT NULL AUTO_INCREMENT , site_name VARCHAR(255) , csm_adress VARCHAR(255) , site_login VARCHAR(255)  , site_password VARCHAR(255)  , ftp_host VARCHAR(255)  , ftp_login VARCHAR(255)  , ftp_password VARCHAR(255)  , db_name VARCHAR(255), db_login VARCHAR(255) ,db_password VARCHAR(255) ,more_info TEXT NOT NULL , PRIMARY KEY (id)) ENGINE = InnoDB";
+* $query_II = "INSERT INTO klients_projects( site_name, cms_adress, site_login, site_password, ftp_host, ftp_login, ftp_password, db_name, db_login,db_password,more_info) VALUES ('site name', 'cms url', 'site login', 'site password', 'ftp url', 'ftp login', 'ftp password','db name', 'db login','db password','more info')";
  
 $sth = $dbh->prepare($query);
 $sth->execute();
 */
 
 
-if (strlen($_GET["new-project-input"]) > 0  && strlen($_GET["site-name"]) > 0 ){
+if (strlen($_GET["new_project_input"]) > 0  && strlen($_GET["site_name"]) > 0 ){
     array_pop($_GET);
     $str_values = implode("','",$_GET);
     $str_values = "'".$str_values."'";
-    $query_create_new_project = "INSERT INTO `klients-projects`( `site-name`, `cms-adress`, `site-login`, `site-password`, `ftp-host`, `ftp-login`, `ftp-password`,`db-name`, `db-login`,`db-password`,`more-info`) VALUES ($str_values)";
+    $query_create_new_project = "INSERT INTO klients_projects( site_name, cms_adress, site_login, site_password, ftp_host, ftp_login, ftp_password, db_name, db_login,db_password,more_info) VALUES ($str_values)";
     $sth = $dbh->prepare($query_create_new_project);
     $sth->execute();    
 }
@@ -118,28 +118,28 @@ echo "</pre>";
             <div class="col-md-12">
                 <div class="btn btn-success new-project-btn">Создать новый проект</div>
                     <form class="new-project-form active" action="">                        
-                        <input type="text" name="site-name" placeholder="site-name" value="">
-                        <input type="text" name="cms-adress" placeholder="cms-adress" value="">
-                        <input type="text" name="site-login" placeholder="site-login" value="">
-                        <input type="text" name="site-password" placeholder="site-password" value="">
-                        <input type="text" name="ftp-host" placeholder="ftp-host" value="">
-                        <input type="text" name="ftp-login" placeholder="ftp-login" value="">
-                        <input type="text" name="ftp-password" placeholder="ftp-password" value="">
-                        <input type="text" name="db-name" placeholder="db-name" value="">
-                        <input type="text" name="db-login" placeholder="db-login" value="">
-                        <input type="text" name="db-password" placeholder="db-password" value="">
-                        <textarea name="more-info"></textarea>
-                        <button type="submit" name="new-project-input" value="1" class="btn btn-info">Сохранить</button>
+                        <input type="text" name="site_name" placeholder="site_name" value="">
+                        <input type="text" name="cms_adress" placeholder="cms_adress" value="">
+                        <input type="text" name="site_login" placeholder="site_login" value="">
+                        <input type="text" name="site_password" placeholder="site_password" value="">
+                        <input type="text" name="ftp_host" placeholder="ftp_host" value="">
+                        <input type="text" name="ftp_login" placeholder="ftp_login" value="">
+                        <input type="text" name="ftp_password" placeholder="ftp_password" value="">
+                        <input type="text" name="db_name" placeholder="db_name" value="">
+                        <input type="text" name="db_login" placeholder="db_login" value="">
+                        <input type="text" name="db_password" placeholder="db_password" value="">
+                        <textarea name="more_info"></textarea>
+                        <button type="submit" name="new_project_input" value="1" class="btn btn-info">Сохранить</button>
                     </form>
             </div>
         </div>
     </div>
     <hr>
-    <?php if (strlen($_GET["details-row"]) > 0 ){?>
+    <?php if (strlen($_GET["details_row"]) > 0 ){?>
         <div class="container">
             <div class="row">
                 <div class="col-md-12 bg-warning p-2">
-                    <div class="h1 text-center">Редактирование записи <b><?php echo $details_row[0]["site-name"];?></b></div>
+                    <div class="h1 text-center">Редактирование записи <b><?php echo $details_row[0]["site_name"];?></b></div>
                     <form action="">   
                         <table class="table table-striped table-bordered table-responsive text-center">
                                 <tr>
@@ -163,7 +163,7 @@ echo "</pre>";
                                     <?php endforeach;?>   
                                 </tr>                
                         </table>
-                        <button type="submit" name="save-edit-row" class="btn btn-light" >Сохранить изменения</button>
+                        <button type="submit" name="save_edit_row" class="btn btn-light" >Сохранить изменения</button>
                 </form>
                 </div>
             </div>
@@ -174,7 +174,7 @@ echo "</pre>";
         <div class="col-md-3 bg-light d-flex align-items-center flex-column">
             <?php
                 foreach($show_names_pass as $name){                    
-                    echo "<div class='m-1 p-1'>".$name["site-name"]."</div>";
+                    echo "<div class='m-1 p-1'>".$name["site_name"]."</div>";
                 }
             ?>
         </div>     
@@ -215,12 +215,12 @@ echo "</pre>";
                     ?>
                     <td class="ctrl-btns">
                         <form action="">
-                            <button class="btn btn-light" name="details-row" value="<?php echo $array["id"] ?>" type="submit">                                
+                            <button class="btn btn-light" name="details_row" value="<?php echo $array["id"] ?>" type="submit">                                
                                 <i class="far fa-arrow-alt-circle-right text-success"></i>
                             </button>                        
                         </form>
                         <form class="d-none" action="">
-                            <button class="btn btn-light" name="delete-row" value="<?php echo $array["id"] ?>" type="submit">
+                            <button class="btn btn-light" name="delete_row" value="<?php echo $array["id"] ?>" type="submit">
                                 <i class="far fa-trash-alt text-danger"></i>
                             </button>
                         </form>
